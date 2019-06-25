a8a655c343f8d0f56447c0d016a003812ff8ba1f
Emily Zheng <eazheng@fb.com>
2019-06-25 13:53:54 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by eazheng on 6/25/19.
+//  Copyright © 2019 eazheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
